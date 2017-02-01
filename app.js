c0b74b7489f411b56d15a67226495b918c6b4ea6
Daniele Costarella <daniele.costarella@gmail.com>
2017-02-01 13:35:58 +0100

    Implemented the function to communicate with the backend.

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 var app = angular.module('RadioApp', []);
 
-app.controller('RadioController', function($scope) {
+app.controller('RadioController', function($scope, $http) {
+
   $scope.message = 'Cheers from Willy :)';
+
+  this.getRadioStations = function()
+  {
+    // Simple GET request example:
+    $http({
+        method: 'GET',
+        url: 'http://0.0.0.0:8080/api/stations'
+    }).then(function successCallback(response) {
+        // this callback will be called asynchronously
+        // when the response is available
+        $scope.message = response;
+    }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        $scope.message = response;
+    });
+
+  };
 });
