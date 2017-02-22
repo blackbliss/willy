7a333929e4f251cdb5f3522ff2e18a8f0a039947
Matteo Sticco <matteo.sticco@gmail.com>
2017-02-22 22:12:21 +0100

    set getRadioStations function into a . Changed (temporarily?) the url address in order to prevent CORS errors.

--- static/app.js
+++ static/app.js
@@ -2,22 +2,22 @@ var app = angular.module('RadioApp', []);
 
 app.controller('RadioController', function($scope, $http) {
 
-  $scope.message = 'Cheers from Willy :)';
+  $scope.greetings = 'Cheers from Willy :)';
 
-  this.getRadioStations = function()
+  $scope.getRadioStations = function()
   {
     // Simple GET request example:
     $http({
         method: 'GET',
-        url: 'http://0.0.0.0:8080/api/stations'
+        url: 'http://localhost:8080/api/stations'
     }).then(function successCallback(response) {
         // this callback will be called asynchronously
         // when the response is available
-        $scope.message = response;
+        $scope.greetings = response.data;
     }, function errorCallback(response) {
         // called asynchronously if an error occurs
         // or server returns response with an error status.
-        $scope.message = response;
+        $scope.greetings = response;
     });
 
   };
